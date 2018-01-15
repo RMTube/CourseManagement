1fe428e5e29e3920ba91ddb67387ab335669453e
ntuladhar <nirala.tuladhar@concur.com>
2018-01-15 10:26:53 -0800

    Course update

--- course.java
+++ course.java
@@ -1,14 +1,50 @@
-public class course
-    private String courseName;
-    
-    
-    
-    public void courseName (string name){
-        course name = science
-        
-    
-    instructur's name
-    
-    total hour to study
-    
-    students name
+/**
+ * Created by ntuladhar on 1/15/18.
+ */
+public class Course {
+    private String name;
+    private String instructorName;
+    private String studentName;
+    private int totalHours;
+
+    //Constructor
+    public Course(String courseName,String instructorName, int totalHours)
+    {
+        this.name = name;
+        this. instructorName = instructorName;
+        this.totalHours = totalHours;
+        this.studentName=""
+    }
+
+    //Accessors
+    public String getName() {
+        return name;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    //Displays all the variables
+    public void Display() {
+        String.format("%s is %d years old.", name, age);
+        System.out.println(String.format("Course Name is %s ", this.name));
+        System.out.println(String.format("Instructor Name is %s ",this.name));
+        System.out.println(String.format("Total Hours requires to Complete the Course is %s ",this.totalHours);
+        if (!this.studentName.isEmpty()) {
+            System.out.println(String.format("Student Name is %s " ,this.name));
+        }
+    }
+}
