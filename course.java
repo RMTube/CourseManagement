a7cca33db76d9a8f911f2f63cf69d746cf141174
ntuladhar <nirala.tuladhar@concur.com>
2018-01-15 09:35:39 -0800

    initial commit

---
+++ course.java
@@ -0,0 +1,14 @@
+public class course
+    private String courseName;
+    
+    
+    
+    public void courseName (string name){
+        course name = science
+        
+    
+    instructur's name
+    
+    total hour to study
+    
+    students name
